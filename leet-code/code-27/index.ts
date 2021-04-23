4de2725a80300458a4a2acefc4bf1a41be032669
yangjnda <yangjinda@bytedance.com>
2021-04-23 21:11:17 +0800

    test

--- leet-code/code-27/index.ts
+++ leet-code/code-27/index.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-23 21:10:35
+ * @LastEditTime: 2021-04-23 21:11:09
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /leet-code/leet-code/code-27/index.ts
+ */
 function removeElement(nums: number[], val: number): number {
   let idx = 0;
 
@@ -15,4 +23,5 @@ console.log(removeElement(nums, val));
 const nums2 = [0, 1, 2, 2, 3, 0, 4, 2],
   val2 = 2;
 console.log(removeElement(nums2, val2));
+// test
 export {};
